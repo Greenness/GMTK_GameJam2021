475489e51345625f73ffa2f6b55737cde66774c1
Patrick Black <ariablack140@gmail.com>
2021-06-12 13:57:56 -0400

    Changes to the values/Added enemy prefab

--- GMTK_GameJam2021_Unity/Assets/Scripts/EnemyMoveLeftRight.cs
+++ GMTK_GameJam2021_Unity/Assets/Scripts/EnemyMoveLeftRight.cs
@@ -6,12 +6,12 @@ public class EnemyMoveLeftRight : MonoBehaviour
 {
     public Rigidbody2D body2D;
     public Transform playerTransform;
-    public float speed = .05f;
+    public float speed = .005f;
     float timeCounter = 0;
     void Update () {
             timeCounter += Time.deltaTime;
             float x = speed * Mathf.Cos (timeCounter);
-            playerTransform.position += new Vector3(x, 0);
+            playerTransform.position += new Vector3(x,0);
     }
 
 
