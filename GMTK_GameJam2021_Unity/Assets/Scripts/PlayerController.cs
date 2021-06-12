3926fb1e09d13b3f021ca37a03a7d088b8018aa0
Patrick Black <ariablack140@gmail.com>
2021-06-12 15:03:27 -0400

    Added Bullet info

--- GMTK_GameJam2021_Unity/Assets/Scripts/PlayerController.cs
+++ GMTK_GameJam2021_Unity/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,9 @@ public class PlayerController : MonoBehaviour
 
     public float health;
     public float massLoss;
+    public GameObject gameControllerInstance;
+    public float bulletSpeed = 15.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -70,12 +73,21 @@ public class PlayerController : MonoBehaviour
             body2D.velocity = trajectory;
             health -= 1;
             recoil += massLoss;
+
+            shootBullet();
         }
 
         //DEATH
         //Logic for when health runs out
-        if (health <= 0) {
+        if (health <= 0)
+        {
             //gameObject.SetActive() = false;
         }
+    }
+
+    void shootBullet()
+    {
+        Vector2 bulletVelocity = bulletSpeed * aim;
+        gameControllerInstance.GetComponent<GameControl>().shootNewBullet(transform.position, bulletVelocity);
     }
 }
